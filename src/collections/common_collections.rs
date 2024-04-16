ff78cec553ba96705cb999c733a4470221b0d60b
illyasen1999 <yuukiquitain@gmail.com>
2024-04-16 20:39:02 +0800

    Continue learning about Common Collections (n:vec)

--- src/collections/common_collections.rs
+++ src/collections/common_collections.rs
@@ -18,5 +18,40 @@ pub fn common_collections() {
     // Rust has a vec![] macro that creates a new vector that holds the values you give it.
     let _value_vec = vec![1, 2, 3];
 
-    // https://doc.rust-lang.org/book/ch08-01-vectors.html#updating-a-vector
+    // Updating a vector
+    // initializng a vector and giving it values
+    // by making the vector mutable we can add values in the vector
+    // vectors are index base so the first element is starting at 0
+    let mut _new_vec = Vec::new();
+
+    // pushing new values in a vectord
+    _new_vec.push(5);
+    _new_vec.push(6);
+    _new_vec.push(7);
+    _new_vec.push(8);
+
+    // outputting values of a vector
+    println!("Vector: {:#?}", &_new_vec);
+
+    for i in &_new_vec {
+        println!("Vec Item: {:?}", &i);
+    }
+
+    // Reading Elements of Vectors
+    // "indexing" - one of the ways of referencing an element in a vector, this works best if you want the program to panic if you are accessing an element past the length of the vector
+    let _another_vec = vec![1, 2, 3, 4, 5];
+    let third_element: &i32 = &_another_vec[2]; // getting the ref
+    println!("Third element is {}", third_element);
+
+     // ".get()" -the other way of referencing a value in a vector, this works best if you want your code to have the logit to handle having either "Some(&element)" or "None"
+    let third_element: Option<&i32> = _another_vec.get(2); // the .get() returns an Option enum
+    match third_element {
+        // running some code if there is a third element
+        Some(third_element) => println!("Third element is {:?}", third_element),
+        // handling the situation where the element that you are lookign for is out-of-bounds of the vector
+        None => println!("There is no third element"),
+    }
+
+    // https://doc.rust-lang.org/book/ch08-01-vectors.html#reading-elements-of-vectors
+
 }
